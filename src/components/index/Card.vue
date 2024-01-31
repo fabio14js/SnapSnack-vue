2dfd08c28f96017ede9eb2eb4064fb3216817bed
Fabio <thebloodshadow14@gmail.com>
2024-01-31 13:03:22 +0100

    ADD - CardsHandler & Card components

---
+++ src/components/index/Card.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  image: String,
+});
+</script>
+
+<template>
+  <a class="flex flex-col justify-between items-center drop-shadow-lg flex-shrink-0 gap-4 h-[200px] bg-[#ff6347ea] rounded-lg">
+    <img draggable="false" class="w-[180px] mt-5 drop-shadow-md h-full object-scale-down p-1 flex-grow" :src="image" alt="Immagine" />
+    <div class="bg-white rounded-b-lg w-full py-2">
+      <h3 class="text-center">Rosticceria</h3>
+    </div>
+  </a>
+</template>
---
+++ src/components/index/CardsHandler.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+import Card from "./Card.vue";
+import { useScroll } from "@vueuse/core";
+
+const images = ["/hamburger.png", "/pasta.png", "/fries.png", "/kebab.png", "/arancini.png", "/pizza.png", "/sushi.png", "/shopping-bag.png"];
+
+const products = [
+  {
+    title: "hamburger",
+    image: "/hamburger.png",
+  },
+];
+</script>
+
+<template>
+  <section class="container my-0 mx-auto transition-all duration-200 px-6">
+    <div class="flex flex-col gap-2">
+      <h2 class="text-3xl font-bold">Le cucine più amate</h2>
+      <span>Trova le cucine più amate dai ristoranti nella tua zona e ordina online a domicilio. </span>
+      <div class="overflow-x-auto whitespace-nowrap mt-4 flex gap-10 items-center overflow-hidden py-4">
+        <Card v-for="(i, index) in 8" :image="images[index]" />
+      </div>
+    </div>
+  </section>
+</template>
