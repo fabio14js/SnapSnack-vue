0382de40bb51d8aa544c23ee8bf2d2c15c3e24de
Fabio <thebloodshadow14@gmail.com>
2024-01-31 17:47:12 +0100

    ADD - cards animation & media

--- src/components/index/Card.vue
+++ src/components/index/Card.vue
@@ -1,14 +1,30 @@
 <script setup>
 defineProps({
+  title: String,
   image: String,
 });
 </script>
 
 <template>
-  <a class="flex flex-col justify-between items-center drop-shadow-lg flex-shrink-0 gap-4 h-[200px] bg-[#ff6347ea] rounded-lg">
-    <img draggable="false" class="w-[180px] mt-5 drop-shadow-md h-full object-scale-down p-1 flex-grow" :src="image" alt="Immagine" />
+  <a
+    href="/"
+    class="flex flex-col cursor-pointer hover:scale-105 transition-all duration-200 justify-between items-center drop-shadow-lg hover:drop-shadow-md lg:flex-shrink-0 gap-4 h-[200px] bg-[#ff6347ea] rounded-lg"
+  >
+    <img
+      id="card_image"
+      draggable="false"
+      class="w-[180px] smooth-transition mt-5 drop-shadow-md h-full object-scale-down p-1 flex-grow"
+      :src="image"
+      alt="Immagine"
+    />
     <div class="bg-white rounded-b-lg w-full py-2">
-      <h3 class="text-center">Rosticceria</h3>
+      <h3 class="text-center text-base">{{ title }}</h3>
     </div>
   </a>
 </template>
+
+<style scoped>
+.smooth-transition {
+  transition: transform 0.1s ease-out;
+}
+</style>
