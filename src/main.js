0925a57880479ad04dc1a5f498a2c4784496429e
Fabio <thebloodshadow14@gmail.com>
2024-01-31 18:42:29 +0100

    FIX

--- src/main.js
+++ src/main.js
@@ -6,9 +6,10 @@ import { router } from "./router";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faUserSecret, faChevronDown, faUtensils, faCircleArrowLeft, faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { faAddressCard, faUser } from '@fortawesome/free-regular-svg-icons'
+import { faAddressCard, faUser } from "@fortawesome/free-regular-svg-icons";
 
 library.add(faUserSecret, faChevronDown, faAddressCard, faUser, faUtensils, faCircleArrowLeft, faCircleArrowRight);
 
 createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
 
+//
