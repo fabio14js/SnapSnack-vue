0e56e5783103fa4270c14c4650eb12e09ef95af9
DavideCavallotto <davidecavallotto92@gmail.com>
2024-02-02 11:33:54 +0100

    ADD - footer card

--- src/components/index/ContactFooter.vue
+++ src/components/index/ContactFooter.vue
@@ -1,36 +1,106 @@
 <script setup>
 
 const restaurantChains = [
-  "McDonald's",
-  "Pizza Hut",
-  "KFC (Kentucky Fried Chicken)",
-  "Burger King",
-  "Roadhouse Grill",
-  "Old Wild West",
-  "La Piadineria",
-  "Bar della Rabbia",
-  "Tigelleria Romana",
-  "Fratelli La Bufala",
+    {
+    name: "McDonald's",
+    link: "https://www.mcdonalds.it/"
+    },
+    {
+    name: "Pizza Hut",
+    link: "https://www.pizzahut.co.uk/"
+    },   
+    {
+    name: "KFC (Kentucky Fried Chicken)",
+    link: "https://www.kfc.it/"
+    },
+    {
+    name: "Burger King",
+    link: "https://www.burgerking.it/index.htm"
+    },
+    {
+    name: "Roadhouse Grill",
+    link: "https://www.roadhouse.it/"
+    },
+    {
+    name: "Old Wild West",
+    link: "https://www.oldwildwest.it/code/22013/home"
+    },
+    {
+    name: "La Piadineria",
+    link: "https://www.lapiadineria.com/"
+    },
+
+    {
+    name: "Tigelleria Romana",
+    link: "https://www.tigelleriaromana.it/"
+    },
+    {
+    name: "Fratelli La Bufala",
+    link: "https://fratellilabufala.com/"
+    },
+    {
+        name: "Con Mollica O Senza",
+        link: "https://www.conmollicaosenzashop.it/"
+    }
+
 ];
+
+const legalNotices = [
+"Termini & Condizioni",
+"Informativa sulla privacy",
+"Cookies",
+"Richieste da parte delle Autorità Pubbliche"
+]
+
+const companyInformation = [
+"Domande frequenti",
+"Chi Siamo",
+"Lavora con noi",
+"Contatti",
+"SnapSnack Pro"
+]
+
 
 </script>
 
 
 <template>
-    <div class="left-0 bottom-0 w-full bg-black text-white grid grid-cols-4 gap-4 place-items-center">
-        <div>
-            <img class="w-[500px]" src="\public\snaps-logo.png" alt="">
+    <div class="transition-all duration-200 container my-0 mx-auto">
+        <div class="w-full flex gap-[200px] items-center">
+           <div class="flex flex-col gap-6">
+                <img class="w-[500px] drop-shadow-md" src="\public\snaps-logo.png" alt="logo"> 
+                <ul class="flex items-center justify-center gap-6 text-[#FFA500] text-3xl">              
+                    <li><a href=""><font-awesome-icon icon="fa-brands fa-facebook-f" /></a></li>
+                    <li><a href=""><font-awesome-icon icon="fa-brands fa-x-twitter" /></a></li>
+                    <li><a href=""><font-awesome-icon icon="fa-brands fa-instagram" /></a></li>
+                    <li><a href=""><font-awesome-icon icon="fa-brands fa-tiktok" /></a></li>                
+                </ul>
+                <span class="text-[#FFA500] text-center text-lg">© 2024 SnapSnack</span>
+                
+    
+            </div>
+            <div class="grid grid-cols-3 gap-4">
+                <ul class="flex flex-col gap-2 bg-white rounded-md p-8 drop-shadow-xl">
+                    <li class="mb-2">
+                        <h2 class="text-[#FFA500] text-2xl font-bold">Catene</h2>
+                    </li>
+                    <li v-for="restaurant in restaurantChains"><a :href="restaurant.link">{{ restaurant.name }}</a></li>
+                </ul>
+                <ul class="flex flex-col gap-2 bg-white rounded-md p-8 drop-shadow-xl">
+                    <li class="mb-2">
+                        <h2 class="text-[#FFA500] text-2xl font-bold">Note legali</h2>
+                    </li>
+                    <li v-for="notice in legalNotices"><a href="">{{ notice }}</a></li>
+                </ul>
+                <ul class="flex flex-col gap-2 bg-white rounded-md p-8 drop-shadow-xl">
+                    <li class="mb-2">
+                        <h2 class="text-[#FFA500] text-2xl font-bold">About Us</h2>
+                    </li>
+                    <li v-for="information in companyInformation"><a href="">{{ information }}</a></li>
+                </ul>            
+            </div>                
+          
         </div>
-        <div>
-            <ul>
-                <li>
-                    <h2>Catene</h2>
-                </li>
-                <li v-for="restaurant in restaurantChains">{{ restaurant }}</li>
-            </ul>
-        </div>
-        <div>ciao</div>
-        <div>ciao</div>
 
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faUserSecret, faChevronDown, faUtensils, faCircleArrowLeft, faCircleArrowRight, faCircleDot } from "@fortawesome/free-solid-svg-icons";
 import { faAddressCard, faUser } from "@fortawesome/free-regular-svg-icons";
+import { faFacebookF, faInstagram, faTiktok, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 
-library.add(faUserSecret, faChevronDown, faAddressCard, faUser, faUtensils, faCircleArrowLeft, faCircleArrowRight, faCircleDot);
+
+library.add(faUserSecret, faChevronDown, faAddressCard, faUser, faUtensils, faCircleArrowLeft, faCircleArrowRight, faCircleDot, faFacebookF, faXTwitter, faInstagram, faTiktok);
 
 createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
 
