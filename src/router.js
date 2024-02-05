256609ccfb4659c424e658e2e55eed464e514bd5
DavideCavallotto <davidecavallotto92@gmail.com>
2024-02-05 19:09:29 +0100

    ADD - restaurant show

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./pages/home.vue";
+import Restaurant from "./pages/restaurant.vue"
 
 const router = createRouter({
   history: createWebHistory(),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: Home,
+    },
+    {
+      path: "/restaurant",
+      name: "restaurant",
+      component: Restaurant,
     },
   ],
 });
