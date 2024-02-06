2734cf6bcd5d0e68d5fdaba8e5ee2f931095d2b0
DavideCavallotto <davidecavallotto92@gmail.com>
2024-02-06 11:47:17 +0100

    FIX - responsive

--- src/components/restaurant/CardRestaurant.vue
+++ src/components/restaurant/CardRestaurant.vue
@@ -1,17 +1,44 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import axios from "axios";
+import { store } from "../../store.js";
+
+
+console.log(store)
+
 
 const restaurant = ref([]);
 const dishes = ref([]);
+const input = ref("");
 
-axios.get(`http://127.0.0.1:8000/api/restaurant/yuzu-sushi`).then((res) => {
+
+
+axios.get(`${store.apiUrl}/restaurant/yuzu-sushi`).then((res) => {
     restaurant.value = res.data.restaurant;
-    console.log(restaurant.value)
+    
 }) 
-axios.get(`http://127.0.0.1:8000/api/dishes`).then((res) => {
+axios.get(`${store.apiUrl}/dishes`).then((res) => {
     dishes.value = res.data.dishes;
     
+    
+}) 
+
+const filteredDishes = computed ( ()=> {
+    if (input.value) {
+        const tempArray = []
+        dishes.value.forEach((dish)=> {
+            
+            if(dish.name.toLowerCase().includes(input.value.toLowerCase())) {
+                tempArray.push(dish)
+            }
+        })
+
+        
+        return tempArray
+    }
+    
+    return dishes.value   
+
 }) 
 
 const isShowingMenu = ref(true);
@@ -24,9 +51,12 @@ const changeShowingStatus = (type) => {
     } 
 }
 
+
+
 </script>
 
 <template>
+    
 <div class="flex flex-col justify-center items-center py-4">
     <div :style="{ backgroundImage: `url(${restaurant.photo})`} " class="bg-cover lg:w-[1200px] md:w-[800px] sm:w-[400px] rounded shadow-lg flex justify-center items-center flex-col border p-6 mb-10">        
         <div class="px-6 py-4 text-center bg-[#F5DEB3]/[.73] rounded-md shadow-lg">
@@ -46,16 +76,28 @@ const changeShowingStatus = (type) => {
         </div>
     </div>
     
-    <div class="lg:w-[1200px] md:w-[800px] sm:w-[400px] border p-4 rounded-full flex justify-around gap-10 text-xl mb-10 shadow-md">
+    <div class="lg:w-[1200px] md:w-[800px] sm:w-[400px] lg:border p-4 lg:rounded-full flex justify-around gap-10 text-xl mb-10 lg:shadow-md">
         <button @click.prevent="changeShowingStatus ('menu')" class="line-orange menu cursor-pointer p-4 relative">Menu</button>
         <button @click.prevent="changeShowingStatus ('info')" class="line-orange info cursor-pointer p-4 relative">Info</button>
     </div>
 
-    <div v-show="isShowingMenu" v-for="dish in dishes" class="flex flex-col justify-center lg:w-[1200px] md:w-[800px] sm:w-[400px] w-[300px] mb-8">
-        <h2 class="font-bold text-3xl mb-2">Tipo di piatto</h2>
+    <div class="relative flex-grow mb-10">
+        <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 text-gray-400">
+            <font-awesome-icon icon="fa-solid fa-utensils" />
+        </div>
+        <input
+        type="text"
+        id="input-group-1"
+        v-model="input"
+        class="z-10 w-full border-4 border-[#FFA500] text-base rounded-lg hover:border-[#FFD700] focus:outline-none block ps-10 p-2.5"
+        placeholder="Cerca un piatto" />
+    </div>
+    
+    <div v-show="isShowingMenu" v-for="dish in filteredDishes" class="flex flex-col justify-center lg:w-[1200px] md:w-[800px] sm:w-[400px] w-[300px] mb-8">
+        <h2 class="font-bold lg:text-3xl text-2xl lg:text-left text-center mb-2">Tipo di piatto</h2>
         <div class="border rounded-md p-6 hover:shadow-lg cursor-pointer transition-all duration-300">
             <img src="" alt="">
-            <p class="text-xl">{{ dish.name }}</p>
+            <p class="sm:text-xl text-lg">{{ dish.name }}</p>
             <span class="text-lg font-bold">{{ dish.price }} &euro;</span>
         </div>
     </div>
@@ -65,6 +107,8 @@ const changeShowingStatus = (type) => {
     </div>
 
 </div>
+
+
 </template>
 
 <style scoped>
