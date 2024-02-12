cd2a218612738f7fdb064a00d9db20cf42247be9
Fabio <thebloodshadow14@gmail.com>
2024-02-12 09:43:45 +0100

    FIX - router

--- src/components/restaurant/CardDish.vue
+++ src/components/restaurant/CardDish.vue
@@ -1,29 +1,17 @@
 <script setup>
-
-import { defineProps, defineEmits } from 'vue';
-
-const { dish } = defineProps(['dish']);
-const emits = defineEmits();
-
-const addToCart = () => {
-// Emetti un evento 'addToCart' con il piatto selezionato
-emits('addToCart', dish);
-};
-
+const { dish } = defineProps(["dish", "counter"]);
 </script>
 
 <template>
-
-<section class="flex flex-col justify-center w-full mb-8 bg-white hover:scale-[1.02] transition-all duration-300">
-    <div @click="addToCart" class="border rounded-md p-6 hover:shadow-lg cursor-pointer transition-all duration-300">    
-    <p class="sm:text-xl text-lg">{{ dish.name }}</p>
-    <span class="text-lg font-bold">{{ dish.price }} &euro;</span>
-    </div>
-</section>
-
+	<section class="flex justify-between w-full mb-8 bg-white hover:scale-[1.02] transition-all duration-300">
+		<div
+			@click="$emit('addToCart', dish)"
+			class="border rounded-md p-6 hover:shadow-lg cursor-pointer transition-all duration-300">
+			<p class="sm:text-xl text-lg">{{ dish.name }}</p>
+			<span class="text-lg font-bold">{{ dish.price }} &euro;</span>
+		</div>
+		<span> {{ counter }}</span>
+	</section>
 </template>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/restaurant/CardRestaurant.vue
+++ src/components/restaurant/CardRestaurant.vue
@@ -3,217 +3,254 @@ import { ref, computed, onMounted } from "vue";
 import axios from "axios";
 import { store } from "../../store.js";
 import CardDish from "./CardDish.vue";
+import { useStorage } from "@vueuse/core";
 
 const restaurant = ref([]);
 const dishes = ref([]);
 const input = ref("");
-const cart = ref([]);
-// let totalCart = Number(localStorage.getItem('cart'));
-let totalCart = 0
+const cart = useStorage("cart", []);
 
+const { slug } = defineProps(["slug"]);
+defineEmits(["addToCart"]);
 
-axios.get(`${store.apiUrl}/restaurant/yuzu-sushi`).then((res) => {
-    restaurant.value = res.data.restaurant;
-});
-axios.get(`${store.apiUrl}/dishes`).then((res) => {
-    dishes.value = res.data.dishes;
+axios.get(`${store.apiUrl}/restaurant/${slug}`).then((res) => {
+	restaurant.value = res.data.restaurant;
+	dishes.value = res.data.dishes;
 });
 
 const filteredDishes = computed(() => {
-    if (input.value) {
-        const tempArray = [];
-        dishes.value.forEach((dish) => {
-            if (dish.name.toLowerCase().includes(input.value.toLowerCase())) {
-                tempArray.push(dish);
-            }
-        });
+	if (input.value) {
+		const tempArray = [];
+		dishes.value.forEach((dish) => {
+			if (dish.name.toLowerCase().includes(input.value.toLowerCase())) {
+				tempArray.push(dish);
+			}
+		});
 
-        return tempArray;
-    }
+		return tempArray;
+	}
 
-    return dishes.value;
+	return dishes.value;
 });
 
 const isShowingMenu = ref(true);
 const isShowingCart = ref(true);
 
 const toggleCartMenu = () => {
-    isShowingCart.value = !isShowingCart.value;
+	isShowingCart.value = !isShowingCart.value;
 };
 
 const changeShowingStatus = (type) => {
-    if (type === "info") {
-        isShowingMenu.value = false;
-    } else if (type === "menu") {
-        isShowingMenu.value = true;
-    }
+	if (type === "info") {
+		isShowingMenu.value = false;
+	} else if (type === "menu") {
+		isShowingMenu.value = true;
+	}
 };
 
-const addToCartHandler = (dish) => {    
-    // Verifica se il piatto è già presente nel carrello
-    const existingDish = cart.value.find(cartDish => cartDish.id === dish.id);
+const total = computed(() => {
+	let temp = 0;
+	cart.value.forEach((item) => {
+		temp += Number(item.price * item.counter);
+	});
+	return temp.toFixed(2);
+});
 
-    if (existingDish) {        
+const cartCounter = (dish) => {
+	const cartDish = cart.value.find((item) => item.id === dish.id);
+	if (cartDish) {
+		return cartDish.counter;
+	}
+	return 0;
+};
 
-        // Se il piatto è già presente, incrementa il suo contatore
-        if (existingDish.counter < 100) {
-            
-            existingDish.counter++;
-        }        
+const addToCartHandler = (dish) => {
+	if (isShowingCart.value) {
+		toggleCartMenu();
+	}
 
-    } else {
-        // Se il piatto non è presente, aggiungilo al carrello con contatore iniziale 1
-        cart.value.push({ ...dish, counter: 1 });
-    }
-    console.log('cart valore',cart.value)
-    totalCart += Number(dish.price);
-    // totalCart = sum;
-    // localStorage.setItem('cart', sum.toString());          
+	const existingDish = cart.value.find((cartDish) => cartDish.id === dish.id);
 
-   
+	if (existingDish) {
+		if (existingDish.counter < 100) {
+			existingDish.counter++;
+		}
+	} else {
+		cart.value.push({ ...dish, counter: 1 });
+	}
 };
 
 const removeFromCartHandler = (dish) => {
-    if (dish.counter > 1) {
-        dish.counter--;
-        totalCart = totalCart - Number(dish.price);
-    } else {
-        removeDish(dish)
-    }
-   
-}
-
-
+	if (dish.counter > 1) {
+		dish.counter--;
+	} else {
+		removeDish(dish);
+	}
+};
 
 const removeDish = (dish) => {
-    const currentIndexDish = cart.value.findIndex(cartDish => cartDish.id === dish.id);
-    if (currentIndexDish >= 0 ) {       
-        
-        const sumToRemove = cart.value[currentIndexDish].price * cart.value[currentIndexDish].counter        
-        totalCart = totalCart - sumToRemove
-        cart.value.splice(currentIndexDish, 1)        
-    }   
-}
-
-
-
-onMounted(() => {
-    // if (!localStorage.getItem('cart')) {
-    //     localStorage.setItem('cart', '0')
-    // }
-})
-
-
-
-
-
+	const currentIndexDish = cart.value.findIndex((cartDish) => cartDish.id === dish.id);
+	if (currentIndexDish >= 0) {
+		cart.value.splice(currentIndexDish, 1);
+	}
+};
 </script>
 
-<template>    
-    <section class="bg-image">
-        <div class="container mx-auto transition-all duration-200 px-12 relative">
-            <div class="flex flex-col justify-center items-center py-4">
-                <div :style="{ backgroundImage: `url(${restaurant.photo})` }"
-                    class="bg-no-repeat bg-cover transition-all duration-200 2xl-[1200px] lg:w-[1000px] md:w-[800px] sm:w-[600px] w-[400px] rounded shadow-lg flex justify-center items-center flex-col border p-6 mb-10">
-                    <div class="px-6 py-4 text-center bg-[#F5DEB3]/[.73] rounded-md shadow-lg">
-                        <h1 class="font-bold text-6xl mb-2">{{ restaurant.name }}</h1>
-                        <p class="text-black text-xl">
-                            {{ restaurant.address }}
-                        </p>
-                    </div>
-                    <div class="bg-zinc-300/[.73] rounded-md p-2 m-2 text-sm flex gap-4 shadow-lg">
-                        <p>Costo Spedizione <span class="font-bold">3,99 $</span></p>
-                        <p>Ordina minimo <span class="font-bold">5,99 $</span></p>
-                    </div>
-                    <div class="px-6 pt-4 pb-2">
-                        <span v-for="category in restaurant.categories"
-                            class="inline-block bg-[#fe6346] rounded-full px-6 py-1 text-sm font-semibold text-white mr-2 mb-2 shadow-lg">
-                            {{ category.name }}
-                        </span>
-                    </div>
-                </div>
-                <!-- Cart -->
-                <div class="absolute top-4 right-0 bg-white rounded-md p-3 shadow-lg border-2 cursor-pointer" @click="toggleCartMenu" v-show="isShowingCart">
-                    <font-awesome-icon class="text-3xl" icon="fa-solid fa-cart-shopping" />
-                    
-                </div>
-                <div class="fixed top-0 right-0 bg-black opacity-75 text-white rounded-md lg:shadow-md px-[70px] py-[20px] h-full z-10 flex flex-col justify-between" v-show="!isShowingCart">
-                    <div>
-                        <h2 class="font-semibold text-2xl mb-4">Il tuo Carrello</h2>
-                        <div v-for="dish in cart" :key="dish.id" class="flex justify-between gap-6">
-                            <p>{{ dish.name }}</p>
-                            <p>{{ dish.price }}</p>
-                            <div class="flex gap-2">
-                                <span @click="removeFromCartHandler(dish)" class="text-gray-400 cursor-pointer"><font-awesome-icon icon="fa-regular fa-square-minus" /></span>
-                                <span>{{ dish.counter }}</span>
-                                <span @click="addToCartHandler(dish)" class="text-gray-400 cursor-pointer"><font-awesome-icon icon="fa-regular fa-square-plus" /></span>
-                                <span @click="removeDish(dish)" class="cursor-pointer text-red-500"><font-awesome-icon icon="fa-solid fa-trash-can" /></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flex flex-col gap-4">
-                        <h3 class="font-semibold text-lg">Totale</h3>
-                        <span>{{ totalCart.toFixed(2) }}</span>
-                        <button class="border rounded-md" @click="toggleCartMenu">Chiudi</button>
-                    </div>
-                </div>
-                <!-- Cart -->
+<template>
+	<section class="bg-image">
+		<div class="container mx-auto transition-all duration-200 px-12 relative">
+			<div class="flex flex-col justify-center items-center py-4">
+				<div
+					:style="{ backgroundImage: `url(${restaurant.photo})` }"
+					class="bg-no-repeat bg-cover transition-all duration-200 2xl-[1200px] lg:w-[1000px] md:w-[800px] sm:w-[600px] w-[400px] rounded shadow-lg flex justify-center items-center flex-col border p-6 mb-10">
+					<div class="px-6 py-4 text-center bg-[#F5DEB3]/[.73] rounded-md shadow-lg">
+						<h1 class="font-bold text-6xl mb-2">{{ restaurant.name }}</h1>
+						<p class="text-black text-xl">
+							{{ restaurant.address }}
+						</p>
+					</div>
+					<div class="bg-zinc-300/[.73] rounded-md p-2 m-2 text-sm flex gap-4 shadow-lg">
+						<p>Costo Spedizione <span class="font-bold">3,99 $</span></p>
+						<p>Ordina minimo <span class="font-bold">5,99 $</span></p>
+					</div>
+					<div class="px-6 pt-4 pb-2">
+						<span
+							v-for="category in restaurant.categories"
+							class="inline-block bg-[#fe6346] rounded-full px-6 py-1 text-sm font-semibold text-white mr-2 mb-2 shadow-lg">
+							{{ category.name }}
+						</span>
+					</div>
+				</div>
+				<!-- Cart -->
+				<div
+					class="absolute top-4 right-0 bg-white rounded-md p-3 shadow-lg border-2 cursor-pointer"
+					@click="toggleCartMenu"
+					v-show="isShowingCart">
+					<font-awesome-icon
+						class="text-3xl"
+						icon="fa-solid fa-cart-shopping" />
+				</div>
+				<div
+					class="fixed top-0 right-0 bg-slate-800 opacity-75 text-white w-[460px] lg:shadow-md px-[30px] py-[20px] h-full z-10 flex flex-col justify-between"
+					v-show="!isShowingCart">
+					<div>
+						<h2 class="font-semibold text-[#fe6146] text-2xl mb-4">Il tuo Carrello</h2>
+						<div
+							v-for="dish in cart"
+							:key="dish.id"
+							class="flex mt-8 items-center justify-between gap-10">
+							<span class="flex-grow">{{ dish.name }}</span>
+							<span class="flex-shrink">{{ dish.price }}</span>
+							<div class="flex gap-2 flex-shrink">
+								<span
+									@click="removeFromCartHandler(dish)"
+									class="text-gray-400 cursor-pointer"
+									><font-awesome-icon icon="fa-regular fa-square-minus"
+								/></span>
+								<input
+									min="0"
+									max="100"
+									class="w-[35px] text-center appearance-none bg-transparent"
+									v-model="dish.counter"
+									type="text" />
+								<span
+									@click="addToCartHandler(dish)"
+									class="text-gray-400 cursor-pointer"
+									><font-awesome-icon icon="fa-regular fa-square-plus"
+								/></span>
+								<span
+									@click="removeDish(dish)"
+									class="cursor-pointer text-red-500"
+									><font-awesome-icon icon="fa-solid fa-trash-can"
+								/></span>
+							</div>
+						</div>
+					</div>
+					<div class="flex flex-col gap-4">
+						<h3 class="font-semibold text-lg">Totale</h3>
+						<span>{{ total }}</span>
+						<button
+							class="border rounded-md"
+							@click="toggleCartMenu">
+							Chiudi
+						</button>
+					</div>
+				</div>
+				<!-- Cart -->
 
-                <div class="border w-[80%] p-4 rounded-2xl flex justify-around gap-10 text-xl mb-10 shadow-md bg-white">
-                    <button @click.prevent="changeShowingStatus('menu')"
-                        class="line-orange menu cursor-pointer p-4 relative hover:font-semibold">
-                        Menu
-                    </button>
-                    <button @click.prevent="changeShowingStatus('info')"
-                        class="line-orange info cursor-pointer p-4 relative hover:font-semibold">
-                        Info
-                    </button>
-                </div>
+				<div class="border w-[80%] p-4 rounded-2xl flex justify-around gap-10 text-xl mb-10 shadow-md bg-white">
+					<button
+						@click.prevent="changeShowingStatus('menu')"
+						class="line-orange menu cursor-pointer p-4 relative hover:font-semibold">
+						Menu
+					</button>
+					<button
+						@click.prevent="changeShowingStatus('info')"
+						class="line-orange info cursor-pointer p-4 relative hover:font-semibold">
+						Info
+					</button>
+				</div>
 
-                <div v-show="isShowingMenu" class="relative flex justify-center mb-10">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 text-gray-400">
-                        <font-awesome-icon icon="fa-solid fa-utensils" />
-                    </div>
-                    <input type="text" id="input-group-1" v-model="input"
-                        class="border-4 border-[#FFA500] rounded-lg w-full hover:border-[#FFD700] focus:outline-none block ps-10 p-2.5 font-medium lg:w-96"
-                        placeholder="Cerca un piatto" />
-                </div>
+				<div
+					v-show="isShowingMenu"
+					class="relative flex justify-center mb-10">
+					<div class="absolute inset-y-0 start-0 flex items-center ps-3.5 text-gray-400">
+						<font-awesome-icon icon="fa-solid fa-utensils" />
+					</div>
+					<input
+						type="text"
+						id="input-group-1"
+						v-model="input"
+						class="border-4 border-[#FFA500] rounded-lg w-full hover:border-[#FFD700] focus:outline-none block ps-10 p-2.5 font-medium lg:w-96"
+						placeholder="Cerca un piatto" />
+				</div>
 
-                <CardDish v-show="isShowingMenu" v-for="dish in filteredDishes" :key="dish.id" :dish="dish"
-                    @addToCart="addToCartHandler" />
-                <div v-show="!isShowingMenu">sezione info</div>
-            </div>
-        </div>
-    </section>
+				<section
+					v-show="isShowingMenu"
+					v-for="dish in filteredDishes"
+					:key="dish.id"
+					@click="addToCartHandler(dish)"
+					class="flex justify-between w-full mb-8 bg-white hover:scale-[1.02] transition-all duration-300">
+					<div class="border rounded-md p-6 hover:shadow-lg cursor-pointer transition-all duration-300">
+						<p class="sm:text-xl text-lg">{{ dish.name }}</p>
+						<span class="text-lg font-bold">{{ dish.price }} &euro;</span>
+					</div>
+					<span> {{ cartCounter(dish) }}</span>
+				</section>
+
+				<div v-show="!isShowingMenu">sezione info</div>
+			</div>
+		</div>
+	</section>
 </template>
 
 <style scoped>
 :hover.line-orange::after {
-    content: "";
-    display: block;
-    width: 50%;
-    bottom: 0;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    height: 3px;
-    background-color: #ff6246;
-    position: absolute;
+	content: "";
+	display: block;
+	width: 50%;
+	bottom: 0;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	height: 3px;
+	background-color: #ff6246;
+	position: absolute;
 }
 
 .bg-image {
-    position: relative;
+	position: relative;
 }
 
 .bg-image::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background-image: url(/foods-pattern-bw.jpg);
-    background-size: 250px;
-    opacity: 0.1;
-    z-index: -1;
+	content: "";
+	position: absolute;
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	background-image: url(/foods-pattern-bw.jpg);
+	background-size: 250px;
+	opacity: 0.1;
+	z-index: -1;
 }
 </style>
