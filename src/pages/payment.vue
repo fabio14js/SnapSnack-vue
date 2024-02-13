af23240bac7feb84c416cbe8f39d63b3b1ade362
LoreWebDev24 <lorenzo.delre12@gmail.com>
2024-02-13 17:04:52 +0100

    ADD - form user

---
+++ src/pages/payment.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+import UserData from "../components/payment/UserData.vue";
+
+defineEmits (['customSubmit']);
+
+function handleUserDataSubmit(formData) {
+    console.log(formData);
+}
+
+</script>
+
+<template>
+    <UserData @customSubmit="handleUserDataSubmit" />
+</template>
+
+
+<style scoped></style>
